555dfe6f8ee340280e6d42a43a97e8c4513f0e67
SocNetMigration <67483981+SocNetMigration@users.noreply.github.com>
2023-04-28 15:01:57 +0200

    Update dirichlet_rnd.m

--- dirichlet_rnd.m
+++ dirichlet_rnd.m
@@ -4,6 +4,8 @@ function [X] = dirichlet_rnd(alpha, K)
 %
 % Generates K independent random vectors with from the Dirichlet
 % distribution with parameters alpha
+% 
+% Last update 28 April 2023
 
 % Generate the random variables from the Gamma distribution
 Y = gamrnd(repmat(alpha, K, 1), ones(K, length(alpha)));
